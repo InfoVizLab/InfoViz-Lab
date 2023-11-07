398075bd594bdd9c476aa3deae46bc7483c6e732
InfoVizLab <InfoVizLab.kim@gmail.com>
2023-11-07 18:34:55 +0900

    upload

--- assets/js/animaition.js
+++ assets/js/animaition.js
@@ -1,4 +1,34 @@
 $(document).ready(function() {
+  /* 1 */
+  $(window).scroll( function(){
+      /* 2 */
+      $('.hideme').each( function(i){
+          var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+          var bottom_of_window = $(window).scrollTop() + $(window).height();
+          /* 3 */
+          if( bottom_of_window > bottom_of_object/2 ){
+              $(this).animate({'opacity':'1'},500);
+          }
+      }); 
+  });
+});
+
+$(document).ready(function() {
+  $(window).scroll( function(){
+      $('.slideinleft').each( function(i){
+          
+          var bottom_of_element = $(this).offset().top + $(this).outerHeight();
+          var bottom_of_window = $(window).scrollTop() + $(window).height();
+          
+          if( bottom_of_window > bottom_of_element ){
+              $(this).animate({'margin-left':'0px'},1000);
+          }
+          
+      }); 
+  });
+});
+
+$(document).ready(function() {
   $(window).scroll( function(){
       $('.fadeinleft').each( function(i){
           
