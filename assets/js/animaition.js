37019ab32ff64494367f7578d205c88af6bed967
InfoVizLab <InfoVizLab.kim@gmail.com>
2023-11-07 18:17:04 +0900

    upload

--- assets/js/animaition.js
+++ assets/js/animaition.js
@@ -1,11 +1,5 @@
 // @ts-check
 // @ts-ignore
-import bezierEasing from "https://cdn.skypack.dev/bezier-easing@2.1.0";
-
-const ease = bezierEasing(0.25, 0.1, 0.25, 1.0);
-const easeIn = bezierEasing(0.38, 0.01, 0.78, 0.13);
-const midSlow = bezierEasing(0, 0.7, 1, 0.3);
-
 const def = new Map([
   [
     "slide1",
