42b0aebfab71dcaf7fe7d427b356724679abfe33
InfoVizLab <InfoVizLab.kim@gmail.com>
2023-11-20 17:04:59 +0900

    load

--- assets/js/animaition.js
+++ assets/js/animaition.js
@@ -1,21 +1,20 @@
-const options = {
-    root: null, // viewport
-    rootMargin: "0px",
-    threshold: .5,  // 50%가 viewport에 들어와 있어야 callback 실행
-  }
-  
-  const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-      console.log(entry.isIntersecting);
-      if (entry.isIntersecting) {
-        entry.target.classList.add("active");
-      } else {
-        entry.target.classList.remove("active");
-      }
-    });
-  }, options);
-  
-  const titleList = document.querySelectorAll('h2');
-  
-  // 반복문을 돌려 모든 DOM에 적용
-  titleList.forEach(el => observer.observe(el));
+const wrap = document.getElementsByClassName('wrap')[0]; // 보일 영역
+const container = document.getElementsByClassName('container');
+let page = 0; // 영역 포지션 초기값
+const lastPage = container.length - 1; // 마지막 페이지
+
+window.addEventListener('wheel',(e)=>{
+    e.preventDefault();
+    if(e.deltaY > 0){
+        page++;
+    }else if(e.deltaY < 0){
+        page--;
+    }
+    if(page < 0){
+        page=0;
+    }else if(page > lastPage){
+        page = lastPage;
+    }
+    console.log(e.deltaY)
+    wrap.style.top = page * -100 + 'vh';
+},{passive:false}); // 디폴트 기능 제거 - 스크롤
