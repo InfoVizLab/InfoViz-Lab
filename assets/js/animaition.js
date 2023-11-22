0e04df54ef45fe5ae184f3dec0daabb41ff35038
InfoVizLab <InfoVizLab.kim@gmail.com>
2023-11-22 15:51:45 +0900

    load

--- assets/js/animaition.js
+++ assets/js/animaition.js
@@ -3,6 +3,11 @@ $(document).ready(function(){
   //window의 스크롤창을 변수에 저장  
   var st = $(this).scrollTop();
   $('h1').text(st);
+  if(st > 5){
+    $('.text1').stop().animate({top:100});
+  } else{
+    $('.text1').stop().animate({top:-250});
+  }
   if(st > 700){
     $('.box1').stop().animate({top:300});
   } else{
